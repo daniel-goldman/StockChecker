33d3decbff22bb78b2b3504c6dbcb07eb453d8e5
Daniel Goldman <Daniel@Daniels-MacBook-Pro.local>
2017-02-06 22:22:15 -0500

    Added logic to clear all stock app notifications upon user opening the app

--- StockChecker/AppDelegate.swift
+++ StockChecker/AppDelegate.swift
@@ -39,6 +39,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+        // Clear any notifications upon opening the app
+        let notificationController = NotificationController()
+        notificationController.center.removeAllDeliveredNotifications()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
