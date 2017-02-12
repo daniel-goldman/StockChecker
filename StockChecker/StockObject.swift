ee8bfa09d818fbe0fb3ef55f12414edaadce8403
Daniel Goldman <dgoldmantech@gmail.com>
2017-02-11 22:00:23 -0500

    Updated to include new fields

--- StockChecker/StockObject.swift
+++ StockChecker/StockObject.swift
@@ -11,16 +11,20 @@ class StockObject {
 	var stockTicker: String?
     var lowPrice: String?
     var highPrice: String?
+	var creationDate: String?
+	var pollCountSinceCreated = "0"
     var lastPollData = LastPollData()
 
     init() {
         
     }
 	
-    init(stockTicker: String, lowPrice: String, highPrice: String) {
+	init(stockTicker: String, lowPrice: String, highPrice: String, pollCountSinceCreated: String, creationDate: String) {
         self.stockTicker = stockTicker
         self.lowPrice = lowPrice
         self.highPrice = highPrice
+		self.creationDate = creationDate
+		self.pollCountSinceCreated = pollCountSinceCreated
         self.lastPollData = LastPollData()
     }
 }
